b569a2bf93e5ea8f760e85f84aa29a464bc8123b
renesanger <renejsanger@gmail.com>
2025-04-20 23:52:47 -0400

    Update 48-rotate-image.cs

--- 48-rotate-image.cs
+++ 48-rotate-image.cs
@@ -1,10 +1,32 @@
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        # Reverse the matrix vertically
-        matrix.reverse()
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        (int left, int right) = (0, matrix.Length -1);
+        
+        while(left < right) {
+            var limit = right - left;
+            for(var i = 0; i < limit; i++) {
+                (int top, int bottom) = (left, right);
+                
+                // save the top left position
+                var topLeft = matrix[top][left + i];
+                
+                // put the bottom left value to the top left position
+                matrix[top][left + i] = matrix[bottom - i][left];
+                
+                // put the bottom right value to the bottom left position
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                
+                // put the top right value to the bottom right position
+                matrix[bottom][right - i] = matrix[top + i][right];                 
 
+                // put the saved top left value to the top right position
+                matrix[top + i][right] = topLeft;    
 
-        # Transpose the matrix
-        for i in range(len(matrix)):
-            for j in range(i + 1, len(matrix)):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            }
+            left++;
+            right--;
+        }
+        
+        return;
+    }
+}
