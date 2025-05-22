842d9d03b7b135d4fb6baa998e82244ccba9a134
renesanger <renejsanger@gmail.com>
2025-05-22 11:22:41 -0400

    Create 141-linked-list-cycle1.cs

---
+++ 141-linked-list-cycle1.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow.Equals(fast))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
